27ab9ff9fbd22c539097c0442d6dd791b76a7635
Trinath Sai Subhash Reddy Pittala <subhash8111999@gmail.com>
2025-03-17 12:32:32 -0400

    added signup orm and  api endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const helloRoute = require("./routes/hello");
+const authRouter = require("./routes/auth");
 const mongoose = require("mongoose");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const DB = process.env.MONGODBURI;
 
-app.use(helloRoute);
+app.use(express.json());
+app.use(authRouter);
 
-mongoose.connect(DB).then(()=>{
-    console.log('Mongodb Connected')
-})
+mongoose.connect(DB).then(() => {
+  console.log("Mongodb Connected");
+});
 
 app.listen(PORT, "0.0.0.0", function () {
   console.log(`server running on ${PORT}`);
